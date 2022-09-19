9625eb57ddda2ee1471dd527bacc776ce6843cee
Tejesh Kumar S <Tejesh Kumar S>
2022-09-20 00:04:20 +0530

    Tested Bugs and Resolved

--- db.c
+++ db.c
@@ -27,6 +27,8 @@ typedef enum
 typedef enum
 {
     PREPARE_SUCESS,
+    PREPARE_STRING_TOO_LONG,
+    PREPARE_NEGATIVE_ID,
     PREPARE_SYNTAX_ERROR,
     PREPARE_UNRECOGNIZED_STATEMENT
 } PreparedResult;
@@ -42,8 +44,8 @@ typedef enum
 
 typedef struct{
     uint32_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    char username[COLUMN_USERNAME_SIZE+1];
+    char email[COLUMN_EMAIL_SIZE+1];
 } Row;
 
 typedef struct
@@ -60,8 +62,8 @@ const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
 
 //serialized
 const uint32_t ID_OFFSET = 0;
-const uint32_t USERNAME_OFFSET = ID_OFFSET + USERNAME_SIZE;
-const uint32_t EMAIL_OFFSET = ID_OFFSET + USERNAME_SIZE + EMAIL_SIZE;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
 const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
 //table
@@ -141,17 +143,45 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table* table)
     }
 }
 
+PreparedResult prepared_insert(InputBuffer *input_buffer, Statement *statement){
+    statement->type = STATEMENT_INSERT;
+
+    char* keyword = strtok(input_buffer->buffer," ");
+    char* id_string = strtok(NULL," ");
+    char* username = strtok(NULL," ");
+    char* email = strtok(NULL," ");
+
+    if(id_string==NULL||username==NULL||email==NULL){
+        return PREPARE_SYNTAX_ERROR;
+    }
+
+    int id = atoi(id_string);
+    if(id<0){
+        return PREPARE_NEGATIVE_ID;
+    }
+    if(strlen(username)>COLUMN_USERNAME_SIZE){
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    if(strlen(email)>COLUMN_EMAIL_SIZE){
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username,username);
+    strcpy(statement->row_to_insert.email,email);
+
+    return PREPARE_SUCESS;
+    
+
+
+}
+
 PreparedResult prepared_result(InputBuffer *input_buffer, Statement *statement){
     if(strncmp(input_buffer->buffer, "insert", 6)==0){
-        statement->type = STATEMENT_INSERT;
-
-        int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),statement->row_to_insert.username,statement->row_to_insert.email);
-
-        if(args_assigned<3){
-            return PREPARE_SYNTAX_ERROR;
-        }
-        return PREPARE_SUCESS;
+        return prepared_insert(input_buffer,statement);
     }
+
     else if (strncmp(input_buffer->buffer, "select", 6) == 0){
         statement->type = STATEMENT_SELECT;
         return PREPARE_SUCESS;
@@ -247,6 +277,12 @@ int main(int argc, char const *argv[])
                 continue;
             case(PREPARE_UNRECOGNIZED_STATEMENT):
                 printf("Unrecognized keyword at start of '%s'.\n",input_buffer->buffer);
+                continue;
+            case(PREPARE_STRING_TOO_LONG):
+                printf("String is too long..\n");
+                continue;
+            case(PREPARE_NEGATIVE_ID):
+                printf("ID must be positive");
                 continue;
         }
 
